d873093a0b012d27af8f8e77085c96db5729798b
Berin Aniesh <berinaniesh@gmail.com>
2022-09-10 09:11:08 +0530

    collect files

---
+++ config_dir/fish/config.fish
@@ -0,0 +1,26 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+set -Ux GOPATH /home/berinaniesh/Development/go
+set -Ux PATH $PATH /home/berinaniesh/Development/go/bin /home/berinaniesh/.bin
+set -Ux EDITOR nvim
+
+abbr ytda "youtube-dl -x -f 251"
+abbr ytdaxel "youtube-dl --external-downloader axel --external-downloader-args '-n 8'"
+abbr fishedit "nvim /home/berinaniesh/.config/fish/config.fish"
+abbr lazygit 'git add . && git commit -m "update" && git push origin main'
+abbr swayedit 'nvim /home/berinaniesh/.config/sway/config'
+abbr alacrittyedit 'nvim /home/berinaniesh/.config/alacritty/alacritty.yml'
+alias compile "g++"
+alias nvimedit "nvim ~/.config/nvim/init.vim"
+
+abbr siteupdate "scp -r /home/berinaniesh/Development/berinaniesh.xyz/dst/* berinaniesh@berinaniesh.xyz:/home/berinaniesh/websites/berinaniesh.xyz/"
+
+
+function gitpush
+	git add .
+	git commit -m $argv
+	git push origin main
+	end
